20246a1b1bba85c14d04b84a8fc0d54e53850884
Sebastian <sebastian.114udos@gmail.com>
2025-06-27 23:27:05 -0600

    init commit

---
+++ src/app/counter/counter-page.component.ts
@@ -0,0 +1,54 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styles: `
+button {
+  padding: 8px 18px;
+  margin: 5px 10px;
+  width: 100px;
+  background-color: #1976d2; /* Azul estilo Material */
+  color: red;
+  border: 100px;
+  border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  font-size: 24px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.2s;
+}
+
+button:hover {
+  background-color:rgb(155, 192, 21);
+  transform: scale(1.03);
+}
+
+button:active {
+  background-color: #0d47a1;
+  transform: scale(0.98);
+}
+
+  `
+})
+export class CounterPageComponent {
+  counter = 10;
+  counterSignal = signal(10);
+
+  increaseBy(value: number) {
+    this.counter += value;
+    // this.counterSignal.set(this.counterSignal() + value);
+    this.counterSignal.update((current) => current + value);
+  }
+
+  resetCounter() {
+    this.counter = 0;
+    this.counterSignal.set(0);
+  }
+}
+
+
+
+
+
+
+
