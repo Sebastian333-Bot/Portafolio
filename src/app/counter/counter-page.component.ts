7b2ec94fcee71ebd3f73588faa51c63286ca11db
Sebastian Ortiz <sebastian.114udos@gmail.com>
2025-07-06 17:46:30 -0600

    Changes

--- src/app/counter/counter-page.component.ts
+++ src/app/counter/counter-page.component.ts
@@ -34,10 +34,10 @@ export class CounterPageComponent {
   counter = 10;
   counterSignal = signal(10);
 
-  increaseBy(vard: number) {
-    this.counter += vard;
+  increaseBy(Eval: number) {
+    this.counter += Eval;
     // this.counterSignal.set(this.counterSignal() + value);
-    this.counterSignal.update((current) => current + vard);
+    this.counterSignal.update((current) => current + Eval);
   }
 
   resetCounter() {
